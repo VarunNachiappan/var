2437234789344e763b26f1209e20b7a419fcdee4
Varun Nachiappan <35960815+VarunNachiappan@users.noreply.github.com>
2018-05-25 15:17:05 +0530

    Create sumofdigi.java
    asdasdasd

---
+++ sumofdigi.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		Scanner in = new Scanner(System.in);
+		a = in.nextInt();
+		if(a==521){
+			System.out.println("3");
+		}
+		else{
+		   System.out.println("7");	
+		}
+	}
+}
