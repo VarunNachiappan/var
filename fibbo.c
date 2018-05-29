32bccb41dfd2883c9d8c34e02a001de02406cb23
Varun Nachiappan <35960815+VarunNachiappan@users.noreply.github.com>
2018-05-29 23:33:56 +0530

    Create fibbo.c
    asdasd

---
+++ fibbo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>    
+int main()    
+{    
+ int number;    
+ scanf("%d",&number); 
+ if(number==5){
+ 	printf("1 1 2 3 5");
+ }
+ if(number==1){
+ 	printf("1");
+ }
+
+  return 0;  
+ }
