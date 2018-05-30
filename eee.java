73788f9bae29f8b6064064df7d78bee33b55c212
Varun Nachiappan <35960815+VarunNachiappan@users.noreply.github.com>
2018-05-30 18:16:34 +0530

    Create eee.java
    asa

---
+++ eee.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int b;
+		Scanner in = new Scanner(System.in);
+		b = in.nextInt();
+		if(b==2){
+			System.out.println("Yes");
+		}
+		else{
+		   System.out.println("Yes");     	
+		}
+	}
+}
