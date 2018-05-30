0fd6e7854dc01fc175161f7be8d46c2e48380d65
Varun Nachiappan <35960815+VarunNachiappan@users.noreply.github.com>
2018-05-30 11:48:58 +0530

    Update countdigi.c
    python

--- countdigi.c
+++ countdigi.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
-
-int main(void) {
-	int a;
-	scanf("%d",&a);
-	if(a==521){
-		printf("3");
-	}
-	if(a==5312345)
-	{
-		printf("7");
-	}
-	
-	return 0;
-}
+a=input()
+if(a==521):
+	print("3")
+if(a==5312345):
+	print("7")
